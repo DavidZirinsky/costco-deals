38ad081e8144fea347e96526fbf7c5af38890bb2
David Zirinsky <dzirinsky@gmail.com>
2026-06-19 11:07:54 -0600

    please lint in cd

--- frontend/eslint.config.cjs
+++ frontend/eslint.config.cjs
@@ -2,40 +2,43 @@ const globals = require('globals');
 const tseslint = require('typescript-eslint');
 const pluginReact = require('eslint-plugin-react');
 const pluginReactHooks = require('eslint-plugin-react-hooks');
-const pluginReactRefresh = require('eslint-plugin-react-refresh').default;
 
-module.exports = tseslint.config(
-  {
-    ignores: ['dist', 'node_modules'],
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-    plugins: {
-      react: pluginReact,
-      'react-hooks': pluginReactHooks,
-      'react-refresh': pluginReactRefresh,
-      '@typescript-eslint': tseslint.plugin,
+module.exports = (async () => {
+  const pluginReactRefresh = (await import('eslint-plugin-react-refresh')).default;
+
+  return tseslint.config(
+    {
+      ignores: ['dist', 'node_modules'],
     },
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
+    {
+      files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+      plugins: {
+        react: pluginReact,
+        'react-hooks': pluginReactHooks,
+        'react-refresh': pluginReactRefresh,
+        '@typescript-eslint': tseslint.plugin,
+      },
+      languageOptions: {
+        parser: tseslint.parser,
+        parserOptions: {
+          ecmaFeatures: {
+            jsx: true,
+          },
+        },
+        globals: {
+          ...globals.browser,
         },
       },
-      globals: {
-        ...globals.browser,
+      settings: {
+        react: {
+          version: '18.2.0',
+        },
+      },
+      rules: {
+        ...pluginReact.configs.recommended.rules,
+        ...pluginReactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': 'warn',
       },
     },
-    settings: {
-      react: {
-        version: '18.2.0',
-      },
-    },
-    rules: {
-      ...pluginReact.configs.recommended.rules,
-      ...pluginReactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': 'warn',
-    },
-  },
-);
+  );
+})();
